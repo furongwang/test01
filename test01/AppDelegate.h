e7df340640d89a2dac671ce084c51b208c98a17d
Julian Chen <jinn.j.chen@hotmail.com>
2015-05-27 01:29:23 +0800

    Initial Commit

---
+++ test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test01
+//
+//  Created by BP on 5/27/15.
+//  Copyright (c) 2015 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
